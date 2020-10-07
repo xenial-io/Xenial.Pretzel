4cf240e5d691d990afe085c9f3ce1924106467ce
Manuel Grundner <m.grundner@delegate.at>
2020-10-07 12:45:37 +0200

    build: deploy nuget

--- build/Program.cs
+++ build/Program.cs
@@ -5,11 +5,24 @@ using static Bullseye.Targets;
 
 var sln = "./src/Pretzel.sln";
 var tool = "./src/Pretzel/Pretzel.csproj";
+var configuration = "--configuration Release";
 
 Target("restore", () => RunAsync("dotnet", $"restore {sln}"));
 
-Target("build", DependsOn("restore"), () => RunAsync("dotnet", $"build {sln}"));
-Target("pack", DependsOn("build"), () => RunAsync("dotnet", $"pack {tool}"));
+Target("build", DependsOn("restore"), () => RunAsync("dotnet", $"build {sln} {configuration}"));
+Target("pack", DependsOn("build"), () => RunAsync("dotnet", $"pack {tool} {configuration}"));
+
+Target("deploy.nuget", async () =>
+{
+    var files = Directory.EnumerateFiles("artifacts/nuget", "*.nupkg");
+
+    foreach (var file in files)
+    {
+        await RunAsync("dotnet", $"nuget push {file} --skip-duplicate -s https://api.nuget.org/v3/index.json -k {Environment.GetEnvironmentVariable("NUGET_AUTH_TOKEN")}",
+            noEcho: true
+        );
+    }
+});
 
 Target("default", DependsOn("pack"));
 
